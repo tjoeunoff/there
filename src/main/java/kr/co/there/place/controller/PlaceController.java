fd3124d0a6d4853c2eae7df70fa32db8f2e01556
fineapple-sohi <jshee940315@gmail.com>
2021-11-11 11:08:00 +0900

    [sohi] place관련 홈페이지 jsp 생성

--- src/main/java/kr/co/there/place/controller/PlaceController.java
+++ src/main/java/kr/co/there/place/controller/PlaceController.java
@@ -17,54 +17,75 @@ import kr.co.there.place.model.entity.PlaceVo;
 import kr.co.there.place.service.PlaceService;
 
 @Controller
-@RequestMapping("/admin/place")
+//@RequestMapping("/admin/place")
 public class PlaceController {
 
 	@Autowired
 	PlaceService placeService;
 
-	@GetMapping("")
+	
+	// ===== admin page =====
+	@GetMapping("/admin/place")
 	public String list(Model model) throws Exception {
 		model.addAttribute("list",placeService.list());
 		return "/admin/place/admin_place_list";
 	}
 
-	@GetMapping("/{place_idx}")
+	@GetMapping("/admin/place/{place_idx}")
 	public String detail(@PathVariable("place_idx") int place_idx, Model model) throws SQLException {
 		model.addAttribute("plbean",placeService.One(place_idx, false));
 		return "/admin/place/admin_place_detail";
 	}
 	
-	@GetMapping("/form")
+	@GetMapping("/admin/place/form")
 	public String moveAddPage() {
 		return "/admin/place/admin_place_add";
 	}
 	
-	@PostMapping("")
+	@PostMapping("/admin/place")
 	public String add(Model model, PlaceVo bean) throws SQLException {
 		model.addAttribute("plbean", placeService.add(bean));
 		return "redirect:/admin/place";
 	}
 	
-	@GetMapping("/form/{place_idx}")
+	@GetMapping("/admin/place/form/{place_idx}")
 	public String moveEditPage(@PathVariable("place_idx") int place_idx, Model model) throws SQLException {
 		model.addAttribute("plbean", placeService.One(place_idx, false));
 		return "/admin/place/admin_place_edit";
 	}
 	
-	@PutMapping("/{place_idx}")
+	@PutMapping("/admin/place/{place_idx}")
 	public String edit(@PathVariable("place_idx") int place_idx, PlaceVo bean, Model model) throws SQLException {
 		model.addAttribute("plbean", placeService.edit(bean));
 		return "redirect:/admin/place/{place_idx}";
 	}
 	
-	
-	@DeleteMapping("/{place_idx}")
+	@DeleteMapping("/admin/place/{place_idx}")
 	public String remove(@PathVariable("place_idx") int param) throws SQLException {
 		placeService.remove(param);
 		return "redirect:/admin/place";
 	}
 
-
+	
+	
+	// ===== home page =====
+	@GetMapping("/categroy")
+	public String moveCategoryPage() {
+		return "/home/place/place-by-category";
+	}
+	
+	@GetMapping("/location")
+	public String moveLocationPage() {
+		return "/home/place/place-by-location";
+	}
+	
+//	@GetMapping("/place/{place_idx}")
+	@GetMapping("/place/detail")
+	public String movePlaceDetailPage() {
+		return "/home/place/place-detail";
+	}
+	
+	
+	
 
 }
